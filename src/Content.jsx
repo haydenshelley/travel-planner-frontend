8112cc347135b4d1c1dc35614ad141b070f353df
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-29 09:48:04 -0700

    trip index

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,26 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { Login } from "./Login";
 import { Signup } from "./Signup";
+import { TripsIndex } from "./TripsIndex";
 
 export function Content() {
+  const [trips, setTrips] = useState([]);
+
+  const handleTripsIndex = () => {
+    console.log("handleTripsIndex");
+    axios.get("http://localhost:3000/trips.json").then((response) => {
+      console.log(response.data);
+      setTrips(response.data);
+    });
+  };
+
+  useEffect(handleTripsIndex, []);
+
   return (
     <div>
       <h1>Vacation Planner</h1>
+      <TripsIndex trips={trips} />
       <Signup />
       <Login />
     </div>
---
+++ src/TripsIndex.jsx
@@ -0,0 +1,28 @@
+export function TripsIndex(props) {
+  return (
+    <div>
+      <h1>My Trips</h1>
+      {props.trips.map((trip) => (
+        <div key={trip.id}>
+          <h2>{trip.title}</h2>
+          <img width="400" height="300" src={trip.image_url} />
+          <p>
+            Dates: {trip.start_time} - {trip.end_time}
+          </p>
+          <h2>{trip.title} Activities</h2>
+          {trip.places.map((place) => (
+            <div key={place.id}>
+              <h3>{place.name}</h3>
+              <p>{place.address}</p>
+              <img width="400" height="300" src={place.image_url} />
+              <p>
+                Dates: {place.start_time} - {place.end_time}
+              </p>
+              <p>Description: {place.description}</p>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
