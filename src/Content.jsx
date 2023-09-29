48409cf747e6c7cb051915ae96422ea1101f0f71
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-29 09:31:30 -0700

    frontend authentication, signup, login, logout

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,12 @@
+import { Login } from "./Login";
+import { Signup } from "./Signup";
+
 export function Content() {
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <h1>Vacation Planner</h1>
+      <Signup />
+      <Login />
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,11 @@
+import { LogoutLink } from "./LogoutLink";
+
 export function Header() {
   return (
     <header>
       <nav>
         <a href="#">Home</a> | <a href="#">My Trips</a> |{" "}
-        <a href="#">New Trip</a>
+        <a href="#">New Trip</a> | <LogoutLink />
       </nav>
     </header>
   );
