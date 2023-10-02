4c675760d895958c027a24df630eb2c94cd06614
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-02 13:56:02 -0700

    update end date to beg at start date

--- src/TripsNew.jsx
+++ src/TripsNew.jsx
@@ -1,12 +1,25 @@
 import { Header } from "./Header";
+import { useState } from "react";
 
 export function TripsNew(props) {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const params = new FormData(e.target);
     props.onCreateTrip(params, () => e.target.reset());
     window.location.href = "/trips";
   };
+
+  const handleStartDateChange = (e) => {
+    setStartDate(e.target.value);
+  };
+
+  const handleEndDateChange = (e) => {
+    setEndDate(e.target.value);
+  };
+
   return (
     <div>
       <Header />
@@ -19,10 +32,21 @@ export function TripsNew(props) {
           Image URL: <input name="image_url" type="text" />
         </div>
         <div>
-          Start Date: <input name="start_time" type="datetime-local" />
+          Start Date:{" "}
+          <input
+            name="start_time"
+            type="datetime-local"
+            onChange={handleStartDateChange}
+          />
         </div>
         <div>
-          End Date: <input name="end_time" type="datetime-local" />
+          End Date:{" "}
+          <input
+            name="end_time"
+            type="datetime-local"
+            defaultValue={startDate}
+            onChange={handleEndDateChange}
+          />
         </div>
         <button type="submit">Create Trip</button>
       </form>
