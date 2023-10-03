955c06b9a2004d696eb6425ad6ca07cd91f9827e
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-03 14:11:54 -0700

    location based recommendations page dynamic

--- src/Recommendations.jsx
+++ src/Recommendations.jsx
@@ -4,22 +4,50 @@ import axios from "axios";
 
 export function Recommendations() {
   const [results, setResults] = useState([]);
+  const [location, setLocation] = useState("");
+  const [proximity, setProximity] = useState("");
+  const [category, setCategory] = useState("");
 
   const handleRecommendations = () => {
     axios
-      .get("http://localhost:3000/places/recommendations.json")
+      .get(
+        `http://localhost:3000/places/recommendations.json?location=${location}&proximity=${proximity}&category=${category}`
+      )
       .then((response) => {
         console.log(response.data);
         setResults(response.data.features);
       });
   };
 
-  useEffect(handleRecommendations, []);
-
   return (
     <div>
       <Header />
-      <h1>Recs</h1>
+      <h1>Recommendations</h1>
+      <div>
+        <label>Location:</label>
+        <input
+          type="text"
+          name="location"
+          onChange={(e) => setLocation(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Proximity:</label>
+        <input
+          type="text"
+          name="proximity"
+          onChange={(e) => setProximity(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Category:</label>
+        <input
+          type="text"
+          name="category"
+          onChange={(e) => setCategory(e.target.value)}
+        />
+      </div>
+      <button onClick={handleRecommendations}>Get Activities</button>
       {results.map((result, index) => (
         <div key={index}>
           <h1>{result.properties.name}</h1>
