24fc6c21e869ce5b5891e9b2483b12450c30239d
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-06 13:40:57 -0700

    style login and signup pages

--- src/Login.jsx
+++ src/Login.jsx
@@ -8,6 +8,7 @@ if (jwt) {
 
 export function Login() {
   const [errors, setErrors] = useState([]);
+  const [isErrorVisible, setIsErrorVisible] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -21,31 +22,80 @@ export function Login() {
           "Bearer " + response.data.jwt;
         localStorage.setItem("jwt", response.data.jwt);
         event.target.reset();
-        window.location.href = "/trips"; // Change this to hide a modal, redirect to a specific page, etc.
+        window.location.href = "/trips";
       })
       .catch((error) => {
         console.log(error.response);
         setErrors(["Invalid email or password"]);
+        setIsErrorVisible(true);
       });
   };
 
+  const handleCancel = () => {
+    window.location.href = "/";
+  };
+
   return (
-    <div id="login">
-      <h1>Login</h1>
-      <ul>
-        {errors.map((error) => (
-          <li key={error}>{error}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Email: <input name="email" type="email" />
+    <div
+      id="login"
+      className="container d-flex justify-content-center align-items-center vh-100"
+    >
+      <div className="card" style={{ width: "400px" }}>
+        <div
+          className="card-body"
+          style={{ maxHeight: "800px", overflowY: "auto" }}
+        >
+          <h1 className="card-title text-center">Login</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email:
+              </label>
+              <input
+                name="email"
+                type="email"
+                className="form-control"
+                id="email"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password:
+              </label>
+              <input
+                name="password"
+                type="password"
+                className="form-control"
+                id="password"
+                required
+              />
+            </div>
+            <div className="d-grid gap-2">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={handleCancel}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+          {isErrorVisible && (
+            <div className="mt-3 alert alert-danger">
+              <h5>Error:</h5>
+              <ul>
+                {errors.map((error, index) => (
+                  <li key={index}>{error}</li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
-        <div>
-          Password: <input name="password" type="password" />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      </div>
     </div>
   );
 }
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export function Signup() {
   const [errors, setErrors] = useState([]);
+  const [isErrorVisible, setIsErrorVisible] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -13,38 +14,104 @@ export function Signup() {
       .then((response) => {
         console.log(response.data);
         event.target.reset();
-        window.location.href = "/trips"; // Change this to hide a modal, redirect to a specific page, etc.
+        window.location.href = "/trips";
       })
       .catch((error) => {
         console.log(error.response.data.errors);
         setErrors(error.response.data.errors);
+        setIsErrorVisible(true);
       });
   };
 
+  const handleCancel = () => {
+    window.location.href = "/";
+  };
+
   return (
-    <div id="signup">
-      <h1>Signup</h1>
-      <ul>
-        {errors.map((error) => (
-          <li key={error}>{error}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Name: <input name="name" type="text" />
+    <div
+      id="signup"
+      className="container d-flex justify-content-center align-items-center vh-100"
+    >
+      <div className="card" style={{ width: "400px" }}>
+        <div
+          className="card-body"
+          style={{ maxHeight: "800px", overflowY: "auto" }}
+        >
+          <h1 className="card-title text-center">Signup</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Name:
+              </label>
+              <input
+                name="name"
+                type="text"
+                className="form-control"
+                id="name"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email:
+              </label>
+              <input
+                name="email"
+                type="email"
+                className="form-control"
+                id="email"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password:
+              </label>
+              <input
+                name="password"
+                type="password"
+                className="form-control"
+                id="password"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password_confirmation" className="form-label">
+                Retype Password:
+              </label>
+              <input
+                name="password_confirmation"
+                type="password"
+                className="form-control"
+                id="password_confirmation"
+                required
+              />
+            </div>
+            <div className="d-grid gap-2">
+              <button type="submit" className="btn btn-primary">
+                Signup
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={handleCancel}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+          {isErrorVisible && (
+            <div className="mt-3 alert alert-danger">
+              <h5>Error:</h5>
+              <ul>
+                {errors.map((error, index) => (
+                  <li key={index}>{error}</li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
-        <div>
-          Email: <input name="email" type="email" />
-        </div>
-        <div>
-          Password: <input name="password" type="password" />
-        </div>
-        <div>
-          Password confirmation:{" "}
-          <input name="password_confirmation" type="password" />
-        </div>
-        <button type="submit">Signup</button>
-      </form>
+      </div>
     </div>
   );
 }
