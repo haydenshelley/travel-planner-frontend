3a0ef9cc4f98fb0efa4e46cb3b2510a6ecc8fe10
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-07 14:47:25 -0700

    style trips index

--- src/TripsIndex.jsx
+++ src/TripsIndex.jsx
@@ -47,45 +47,65 @@ export function TripsIndex(props) {
         </div>
       ) : (
         <div>
-          <h1>{props.trips[0].user.name}'s Trips</h1>
-          {sortedTrips.map((trip) => (
-            <div key={trip.id}>
-              <Link to={`/trips/${trip.id}`}>{trip.title}</Link>
-              <br />
-              <img width="400" height="300" src={trip.image_url} />
-              <p>Arrive: {trip.start_time}</p>
-              <p>Depart: {trip.end_time}</p>
-              <div>
-                <label>Invite Friends:</label>
-                <select
-                  onChange={(e) => {
-                    setSelectedFriendId(e.target.value);
-                    setSelectedTripId(trip.id);
-                    console.log("Selected Friend ID:", e.target.value);
-                    console.log("Current Trip ID:", trip.id);
-                  }}
-                  value={selectedFriendId}
-                >
-                  <option value="">Select a friend</option>
-                  {props.allUsers.map((user, index) => (
-                    <option key={index} value={user.id}>
-                      {user.name}
-                    </option>
-                  ))}
-                </select>
-                <button onClick={() => handleInviteFriend()}>
-                  Invite Friend
-                </button>
-                <div>{divContent}</div>
+          <h1>{props.trips[0].user.name.toLowerCase()}'s trips</h1>
+          <div className="card-container">
+            {sortedTrips.map((trip) => (
+              <div key={trip.id} className="card" style={{ width: "25rem" }}>
+                <img
+                  src={trip.image_url}
+                  className="card-img-top"
+                  alt="Trip Image"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">
+                    <Link id="card-title" to={`/trips/${trip.id}`}>
+                      {trip.title}
+                    </Link>
+                  </h5>
+                  <p className="card-text">
+                    {trip.start_time} - {trip.end_time}
+                  </p>
+                  <div>
+                    <select
+                      onChange={(e) => {
+                        setSelectedFriendId(e.target.value);
+                        setSelectedTripId(trip.id);
+                        console.log("Selected Friend ID:", e.target.value);
+                        console.log("Current Trip ID:", trip.id);
+                      }}
+                      value={selectedFriendId}
+                    >
+                      <option value="">Invite a friend</option>
+                      {props.allUsers.map((user, index) => (
+                        <option key={index} value={user.id}>
+                          {user.name}
+                        </option>
+                      ))}
+                    </select>
+                    <button
+                      className="btn custom-color"
+                      onClick={() => handleInviteFriend()}
+                    >
+                      Add Friend
+                    </button>
+                    <div>{divContent}</div>
+                  </div>
+                  <div className="horizontal-buttons">
+                    <a className="btn custom-color" href={`/trips/${trip.id}`}>
+                      Edit Trip
+                    </a>
+                    <br />
+                    <button
+                      className="btn custom-color"
+                      onClick={() => handleDestroyTrip(trip)}
+                    >
+                      Cancel Trip
+                    </button>
+                  </div>
+                </div>
               </div>
-              <Link to={`/trips/${trip.id}/edit`}>Edit Trip</Link>
-              <br />
-              <button onClick={() => handleDestroyTrip(trip)}>
-                Cancel Trip
-              </button>
-              <hr />
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       )}
     </div>
