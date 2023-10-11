98ea0153d37f677d5e871cdb9f0a5c14b50597e9
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-11 13:48:38 -0700

    remove base url from signup

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -10,7 +10,7 @@ export function Signup() {
     setErrors([]);
     const params = new FormData(event.target);
     axios
-      .post("http://localhost:3000/users.json", params)
+      .post("/users.json", params)
       .then((response) => {
         event.target.reset();
         window.location.href = "/trips";
