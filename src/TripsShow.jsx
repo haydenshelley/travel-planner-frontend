72bf525fff4d821f96d1ff06195bfd020a41043f
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-12 13:04:05 -0700

    fix useNavigate

--- src/TripsShow.jsx
+++ src/TripsShow.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams, useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Header } from "./Header";
 import { Link } from "react-router-dom";
 import axios from "axios";
