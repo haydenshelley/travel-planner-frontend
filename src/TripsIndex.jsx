39695c891122084b8d927e7c5ba58177246e9ce0
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-13 10:10:16 -0700

    add conditional to tripsindex for loading

--- src/TripsIndex.jsx
+++ src/TripsIndex.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
 import { Header } from "./Header";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function TripsIndex(props) {
-  const sortedTrips = [...props.trips].sort(
-    (a, b) => new Date(a.start_time) - new Date(b.start_time)
-  );
+  const [sortedTrips, setSortedTrips] = useState([]);
   const [selectedFriendId, setSelectedFriendId] = useState("");
   const [selectedTripId, setSelectedTripId] = useState("");
   const [friendInvited, setFriendInvited] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleInviteFriend = () => {
     axios
@@ -25,6 +24,16 @@ export function TripsIndex(props) {
       });
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      const sortedTrips = [...props.trips].sort(
+        (a, b) => new Date(a.start_time) - new Date(b.start_time)
+      );
+      setSortedTrips(sortedTrips);
+      setLoading(false);
+    }, 1000);
+  }, [props.trips]);
+
   let divContent = null;
   if (friendInvited) {
     divContent = <p>Friend Invited!</p>;
@@ -33,72 +42,82 @@ export function TripsIndex(props) {
   return (
     <div>
       <Header />
-      {props.trips.length === 0 ? (
-        <div>
-          <h1>no current trips</h1>
-          <Link to="/trips/new">try creating a new trip!</Link>
-        </div>
+      {loading ? (
+        <p>Loading...</p>
       ) : (
         <div>
-          <h1>{props.trips[0].user.name}'s trips</h1>
-          <div className="card-container">
-            {sortedTrips.map((trip) => (
-              <div key={trip.id} className="card" style={{ width: "25rem" }}>
-                <img
-                  src={trip.image_url}
-                  className="card-img-top"
-                  alt="Trip Image"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    <Link id="card-title" to={`/trips/${trip.id}`}>
-                      {trip.title}
-                    </Link>
-                  </h5>
-                  <p className="card-text">
-                    {trip.start_time} - {trip.end_time}
-                  </p>
-                  <div>
-                    <select
-                      onChange={(e) => {
-                        setSelectedFriendId(e.target.value);
-                        setSelectedTripId(trip.id);
-                      }}
-                      value={selectedFriendId}
-                    >
-                      <option value="">invite a friend</option>
-                      {props.allUsers.map((user, index) => (
-                        <option key={index} value={user.id}>
-                          {user.name}
-                        </option>
-                      ))}
-                    </select>
-                    <button
-                      className="btn custom-color"
-                      onClick={() => handleInviteFriend()}
-                    >
-                      add friend
-                    </button>
-                    <div>{divContent}</div>
+          {props.trips.length === 0 ? (
+            <div>
+              <h1>no current trips</h1>
+              <Link to="/trips/new">try creating a new trip!</Link>
+            </div>
+          ) : (
+            <div>
+              <h1>{props.trips[0].user.name}'s trips</h1>
+              <div className="card-container">
+                {sortedTrips.map((trip) => (
+                  <div
+                    key={trip.id}
+                    className="card"
+                    style={{ width: "25rem" }}
+                  >
+                    <img
+                      src={trip.image_url}
+                      className="card-img-top"
+                      alt="Trip Image"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        <Link id="card-title" to={`/trips/${trip.id}`}>
+                          {trip.title}
+                        </Link>
+                      </h5>
+                      <p className="card-text">
+                        {trip.start_time} - {trip.end_time}
+                      </p>
+                      <div>
+                        <select
+                          onChange={(e) => {
+                            setSelectedFriendId(e.target.value);
+                            setSelectedTripId(trip.id);
+                          }}
+                          value={selectedFriendId}
+                        >
+                          <option value="">invite a friend</option>
+                          {props.allUsers.map((user, index) => (
+                            <option key={index} value={user.id}>
+                              {user.name}
+                            </option>
+                          ))}
+                        </select>
+                        <button
+                          className="btn custom-color"
+                          onClick={() => handleInviteFriend()}
+                        >
+                          add friend
+                        </button>
+                        <div>{divContent}</div>
+                      </div>
+                      <div className="horizontal-buttons">
+                        <Link
+                          className="btn custom-color"
+                          to={`/trips/${trip.id}/edit`}
+                        >
+                          edit trip
+                        </Link>
+                        <button
+                          className="btn custom-color"
+                          onClick={() => props.handleDestroyTrip(trip)}
+                        >
+                          cancel trip
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="horizontal-buttons">
-                    <Link
-                      className="btn custom-color"
-                      to={`/trips/${trip.id}/edit`}
-                    >
-                      edit trip
-                    </Link>
-                    <button
-                      className="btn custom-color"
-                      onClick={() => props.handleDestroyTrip(trip)}
-                    >
-                      cancel trip
-                    </button>
-                  </div>
-                </div>
+                ))}
               </div>
-            ))}
-          </div>
+            </div>
+          )}
         </div>
       )}
     </div>
